5bd026e976c9d55bae875a0cf9a5128d17ade92b
Woodams R. Clark <ydoow111@gmail.com>
2014-09-03 23:29:31 -0400

    Updated scheduler.py to coincide with updates made to lineUp.py which is now known as songQ.py

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-import schedule, threading
-from lineUp import LineUp
+import schedule, threading, time
+from songQ import SongQ
 
 # Purpose/Function:
 # 	Provide accurate scheduling for playing music
@@ -16,13 +16,14 @@ from lineUp import LineUp
 #	These choices adjust the frequency songs are
 #	played; the amount of time  before playing the next song.
 #	The full ratio is determined with this modifier and
-#	the total length of playtime each lineUp has.
+#	the total length of playtime each songQ has.
 #
 #  ToDo:
 #  	Implement 'Intelligence'
 #  	Notifications: Use a config file to allow user to schedule notifications
 #  				   e.g. At Noon play a specific title
 #  				   Consider how to interrupt music currently playing	
+#  	Use os module to play songs - use os default program to play
 #
 # Notes:
 # 	I'm making use of the schedule module. It seems
@@ -33,23 +34,20 @@ from lineUp import LineUp
 # 	Example: https://github.com/mrhwick/schedule/blob/master/schedule/__init__.py
 
 class Scheduler:
-	lineUp = LineUp()
+	songQ = SongQ(time.localtime()[3])
 
 	def __init__(self):
 		self.scheduleEvents()
 		self.loop()
 
 	def scheduleEvents(self):
-		schedule.every().hour.at(":00").do(self.run_threaded, self.updateCurHour)
+		schedule.every().hour.at(":55").do(self.run_threaded, self.songQ.loadNextSongQ(self.curHour() + 1) )
+		schedule.every().hour.at(":59").do(self.run_threaded, self.songQ.hourlyUpdate)
 		schedule.every(10).minutes.do(self.run_threaded, self.playNextSong)
-#		ToDo: Use lineUp data to play songs periodically
-
-	def updateCurHour(self):
-	# The scheduler calls this method in a newly created thread
-		self.lineUp.setCurHour()
+#		ToDo: Use songQ data to play songs periodically
 
 	def playNextSong(self):
-		# Play{ self.lineUp.nextSong() }
+		# os.play{ self.songQ.nextSong() }
 		# Set flag to indicate song is playing 
 		pass
 
@@ -58,9 +56,12 @@ class Scheduler:
 		func_threaded = threading.Thread(target=func)
 		func_threaded.start()
 
+
 	def loop(self):
 		# Main loop
 		while True:
 			schedule.run_pending()
 
+	def curHour(self):
+		return time.localtime()[3]
 sched = Scheduler()
